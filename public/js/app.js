634c8c79d1b15054b6ab9a5a7a85faf0665d9532
James Little <littleguy23@gmail.com>
2018-01-25 16:00:09 -0500

    A little bit of cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -642,144 +642,13 @@ var _data = {
 		dimension: 15,
 		spaces: [],
 		words: []
-	},
-
-	ca_templates: [{
-		name: "asdf",
-		asciiRep: '...._....._........_....._.............._......._....._............__...._____..._............__....._......_......._......_.....__............_..._____....__............_....._......._.............._....._........_....._....',
-		img: "http://via.placeholder.com/200x200",
-		dimension: "15"
-	}, {
-		name: "asdf",
-		asciiRep: '...._....._........_....._.............._......._....._............__...._____..._............__....._......_......._......_.....__............_..._____....__............_....._......._.............._....._........_....._....',
-		img: "http://via.placeholder.com/200x200",
-		dimension: "15"
-	}, {
-		name: "asdf",
-		asciiRep: '...._....._........_....._.............._......._....._............__...._____..._............__....._......_......._......_.....__............_..._____....__............_....._......._.............._....._........_....._....',
-		img: "http://via.placeholder.com/200x200",
-		dimension: "15"
-	}, {
-		name: "jkla",
-		asciiRep: "...._....._........_....._........_................_.....___..........._...____...__......_..................__...__.................._......__...____..._...........___....._................_........_....._........_....._....",
-		img: "http://via.placeholder.com/200x200",
-		dimension: "15"
-	}]
-
-	// ca_import_string: function(string) {
-	// 	if (Math.sqrt(string.length) % 1 !== 0) {
-	// 	  return;
-	// 	}
-
-	// 	this.ca_board.dimension = Math.sqrt(string.length);
-	// 	this.ca_board.letters = [];
-	// 	this.ca_board.spaces = [];
-
-	// 	if(Math.sqrt(string.length) !== this.ca_board.dimension) {
-	// 	  return;
-	// 	}
-
-	// 	for (var i = 0; i < string.length; i++) {
-	// 		if (string.charAt(i) === "_") {
-	// 			this.ca_board.spaces[i] = {
-	// 				letter: "",
-	// 				isBlackSpace: true
-	// 			}
-	// 		} else if (string.charAt(i) === ".") {
-	// 			this.ca_board.spaces[i] = {
-	// 				letter: "",
-	// 				isBlackSpace: false
-	// 			}
-	// 		} else if (string.charAt(i).match(/[a-z]/i)) {
-	// 			this.ca_board.spaces[i] = {
-	// 				letter: string.charAt(i).toUpperCase(),
-	// 				isBlackSpace: false
-	// 			}
-	// 		}
-	// 		ca_update_canvas();
-	// 	}
-	// },
-
-	// ca_generate_word_list: function() {
-	// 	var oldWords = this.ca_board.words;
-	// 	var spaceCount = this.ca_board.spaces.length;
-	// 	this.ca_board.words = [];
-
-	// 	for (var i = 0; i < spaceCount; i++) {
-	// 		var space = this.ca_board.spaces[i];
-	// 		var spaces = this.ca_board.spaces;
-
-	// 		if (space.isWordStart) {
-	// 			for (var j = 0; j < space.wordStartDirections.length; j++) {
-	// 				var word_array = [i];
-	// 				if (space.wordStartDirections[j] === "h") {
-	// 					var runner = i + 1;
-	// 					while (
-	// 						spaces[runner] &&
-	// 						runner % this.ca_board.dimension != 0 &&
-	// 						!spaces[runner].isBlackSpace
-	// 					) {
-	// 						word_array.push(runner);
-	// 						runner++;
-	// 					}
-	// 				}
-
-	// 				if (space.wordStartDirections[j] === "v") {
-	// 					var runner = i + this.ca_board.dimension;
-	// 					while (
-	// 						spaces[runner] &&
-	// 						runner < Math.pow(this.ca_board.dimension, 2) &&
-	// 						!spaces[runner].isBlackSpace
-	// 					) {
-	// 						word_array.push(runner);
-	// 						runner += this.ca_board.dimension;
-	// 					}
-	// 				}
-	// 				this.ca_board.words.push({
-	// 					startSpace: i,
-	// 					wordStartIndex: space.wordStartIndex,
-	// 					direction: space.wordStartDirections[j],
-	// 					clue: "",
-	// 					spaces: word_array
-	// 				});
-
-	// 				var latest_word_index = this.ca_board.words.length - 1;
-	// 				var latest_word = this.ca_board.words[latest_word_index];
-	// 				var latest_word_spaces_length = latest_word.spaces.length;
-
-	// 				// for every space listed in the lastest word's spaces array
-	// 				for (var p = 0; p < latest_word_spaces_length; p++) {
-	// 					// get the space that the word lists
-	// 					var spaceToConnectToWord = spaces[latest_word.spaces[p]];
-	// 					// and push the latest word's index onto that space's word assoc array
-	// 					spaceToConnectToWord.wordParts.push(latest_word_index);
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-
-	// 	if (oldWords.length == this.ca_board.words.length) {
-	// 		var length = oldWords.length;
-	// 		for (var i = 0; i < length; i++) {
-	// 			this.ca_board.words[i].clue = oldWords[i].clue;
-	// 		}
-	// 	} else {
-	// 		var length = oldWords.length;
-	// 		for (var i = 0; i < length; i++) {
-	// 			var oldValue = ca_value_from_word(oldWords[i]);
-	// 			for (var j = 0; j < this.ca_board.words.length; j++) {
-	// 				if (ca_value_from_word(this.ca_board.words[j]) == oldValue) {
-	// 					this.ca_board.words[j].clue = oldWords[i].clue;
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
+	}
 };
 
 /* harmony default export */ __webpack_exports__["default"] = ({
 	components: {
-		TopBar: __WEBPACK_IMPORTED_MODULE_0__components_TopBar_vue___default.a, PuzzleHeading: __WEBPACK_IMPORTED_MODULE_1__components_PuzzleHeading_vue___default.a, WordColumn: __WEBPACK_IMPORTED_MODULE_2__components_WordColumn_vue___default.a, PuzzleViz: __WEBPACK_IMPORTED_MODULE_3__components_PuzzleViz_vue___default.a, WordSuggestions: __WEBPACK_IMPORTED_MODULE_4__components_WordSuggestions_vue___default.a, BottomBar: __WEBPACK_IMPORTED_MODULE_5__components_BottomBar_vue___default.a
+		TopBar: __WEBPACK_IMPORTED_MODULE_0__components_TopBar_vue___default.a, PuzzleHeading: __WEBPACK_IMPORTED_MODULE_1__components_PuzzleHeading_vue___default.a, WordColumn: __WEBPACK_IMPORTED_MODULE_2__components_WordColumn_vue___default.a, PuzzleViz: __WEBPACK_IMPORTED_MODULE_3__components_PuzzleViz_vue___default.a,
+		WordSuggestions: __WEBPACK_IMPORTED_MODULE_4__components_WordSuggestions_vue___default.a, BottomBar: __WEBPACK_IMPORTED_MODULE_5__components_BottomBar_vue___default.a
 	},
 
 	data: function data() {
@@ -1676,18 +1545,6 @@ exports.push([module.i, "\n.bottom-bar__button {\n  background: rgba(0, 0, 0, 0.
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //
--- resources/assets/js/components/BottomBar.vue
+++ resources/assets/js/components/BottomBar.vue
@@ -9,18 +9,6 @@
 
         <div class="right">
             <a href="#" class="bottom-bar__warning-icon">
-                <!-- <svg width="22px" height="20px" viewBox="0 0 29 27">
-                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <g id="Desktop-HD" transform="translate(-1396.000000, -991.000000)" fill="#FF8D9B" fill-rule="nonzero">
-                            <g id="Bottom-Bar" transform="translate(0.000000, 982.000000)">
-                                <g id="Warning" transform="translate(1396.000000, 9.000000)">
-                                    <path d="M28.8662835,25.47 L15.4122442,0.522 C15.224942,0.1995 14.8775266,0 14.4999012,0 C14.1207652,0 13.7733498,0.1995 13.5875581,0.522 L0.135029416,25.47 C-0.0477412969,25.7865 -0.0447202934,26.1765 0.142581925,26.49 C0.331394646,26.8065 0.674278546,27 1.04586198,27 L27.9539404,27 C28.3240133,27 28.6699182,26.8065 28.8572204,26.49 C29.0445227,26.1765 29.0475437,25.7865 28.8662835,25.47 Z M16,24 L13,24 L13,21 L16,21 L16,24 Z M16,19 L13,19 L13,9 L16,9 L16,19 Z" id="Shape"></path>
-                                </g>
-                            </g>
-                        </g>
-                    </g>
-                </svg> -->
-
                 <svg version="1.1" id="Warning" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                     width="20px" height="20px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve">
                     <path fill="#FF8D9B" d="M19.511,17.98L10.604,1.348C10.48,1.133,10.25,1,10,1C9.749,1,9.519,1.133,9.396,1.348L0.49,17.98
